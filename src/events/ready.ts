3c1ee3039f7c3453100bd7933bae84d1fddbc185
clarkjoao <clark.joao@gmail.com>
2022-04-09 00:43:58 -0300

    feat: ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,7 @@
 import { Client, ClientEvents } from "discord.js";
+import config from "../config";
 
 export default async (events: ClientEvents, client: Client) => {
-
+    const guild = (await client.guilds.fetch(config.bot.guidId))
+    console.log(guild)
 }
