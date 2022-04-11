544a6f40c785c19e2db6694d7a54f36ed8055946
clarkjoao <clark.joao@gmail.com>
2022-04-11 20:11:13 -0300

    feat: add roles to members

---
+++ src/events/guildMemberAdd.ts
@@ -0,0 +1,7 @@
+import { Client, GuildMember } from "discord.js";
+import { Roles } from "models/roles.enum";
+
+export default async (member: GuildMember, client: Client) => {
+  if (member.user.bot) member.roles.add(Roles.BOT);
+  else member.roles.add(Roles.MEMBER);
+};
--- src/events/message.ts
+++
@@ -1,5 +0,0 @@
-import { Client, ClientEvents } from "discord.js";
-
-export default async (events: ClientEvents, client: Client) => {
-
-}
